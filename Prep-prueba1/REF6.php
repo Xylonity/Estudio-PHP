d60ede192495c293b12da72082319f1cd4637c3e
Xylonity <42015327+Xylonity@users.noreply.github.com>
2022-11-01 00:55:53 +0100

    2

---
+++ Prep-prueba1/REF6.php
@@ -0,0 +1,9 @@
+<?php
+
+$array = ["20", "40", "50", "60", "70", "80", "100", "120", "140", "160"];
+
+$res = array_sum($array)/count($array);
+
+printf("La media de los valores del array es: $res");
+
+?>
