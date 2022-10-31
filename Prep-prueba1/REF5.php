cc542d49019a224b22478867e74ae3720a436f18
Xylonity <42015327+Xylonity@users.noreply.github.com>
2022-10-31 18:43:17 +0100

    2

--- Prep-prueba1/REF5.php
+++ Prep-prueba1/REF5.php
@@ -3,8 +3,14 @@
 printf("Dime la cantidad de productos: ");
 $a = (int) readline();
 $array = [];
+$c = 0;
 for($i=0;$i<$a;$i++) {
     $array[$i] = readline("Dime el precio del producto: ");
 }
-printf("La suma es: ".array_sum($array));
+foreach($array as $b) {
+    if ($b > 200) {
+        $c++;
+    }
+}
+printf("La suma es: ".array_sum($array)." y hay $c productos mayores de 200");
 ?>
